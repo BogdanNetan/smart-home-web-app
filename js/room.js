754f0aac72327e45fa46960c8c681a5f37ab1ecc
BogdanNetan <netan.bogdan@gmail.com>
2020-06-15 20:41:51 +0300

    creating update method

--- js/room.js
+++ js/room.js
@@ -31,23 +31,28 @@ window.About = {
     },
 
 
-    deleteRoom: function (id) {
+    updateRoom: function (id, name, targetValue) {
+        let requestBody = {
+            name: name,
+            targetValue: targetValue
+        };
         $.ajax({
-            url: About.API_URL + "/rooms" + "/"+ id,
-            method: "DELETE",
-            contentType: "application/json"
+            url: About.API_URL + "/rooms" + "?id=" + id,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
         }).done(function () {
             About.getRooms();
         })
     },
 
-    getTemperatures: function () {
+    deleteRoom: function (id) {
         $.ajax({
-            url: About.API_URL + "/temperatures",
-            method: "GET"
-        }).done(function (response) {
-            console.log(response);
-            About.displayTemperatures(response.content);
+            url: About.API_URL + "/rooms" + "/" + id,
+            method: "DELETE",
+            contentType: "application/json"
+        }).done(function () {
+            About.getRooms();
         })
     },
 
@@ -59,28 +64,6 @@ window.About = {
         $('#rooms-table tbody').html(roomsHtml);
     },
 
-    displayTemperatures: function (temperatures) {
-        let temperaturesHtml = '';
-
-        temperatures.forEach(temperature => temperaturesHtml += About.getHtmlForOneTemperature(temperature));
-
-        $('.temperatureControl').html(temperaturesHtml);
-    },
-
-
-    updateTemperature: function (id) {
-        let temperaturebody = {
-            targetValue: targetValueValue
-        };
-        $.ajax({
-            url: About.API_URL + "?id=" + id,
-            method: "PUT",
-            contentType: "application/json",
-            data: JSON.stringify(temperaturebody)
-        }).done(function () {
-            About.getTemperatures(id);
-        })
-    },
 
     bindEvents: function () {
         $("#new-room-form").submit(function (event) {
@@ -88,44 +71,55 @@ window.About = {
             About.createRoom();
         });
 
-            $("#rooms-table ").delegate(".delete-room ", "click", function (event) {
+        $("#rooms-table ").delegate(".delete-room ", "click", function (event) {
             event.preventDefault();
             let roomId = $(this).data("id");
             About.deleteRoom(roomId);
         });
+        // $("#rooms-table ").delegate(".edit-room", "click", function (event) {
+        //     event.preventDefault();
+        //     let roomId = $(this).data("id");
+        //
+        //
+        //     $(this).replaceWith($(` <input type="text" data-id="${roomId}" placeholder="Enter new room name"/>`),
+        //         `<input type="submit" value="Save" id=${roomId}  class="submitroomName-mark">`);
 
 
+        $("#rooms-table ").delegate(".Target value", "click", function (event) {
+            event.preventDefault();
+            let roomId = $(this).data("id");
 
-            // $("#temperatureControl").delegate(".button", "click", function (event) {
-            //     event.preventDefault();
-            //     let dataId = $(this).data("id");
-            //     About.updateTemperature();
-            // });
-
-
+            $(this).replaceWith($(` <input type="text" placeholder="Target temperature" data-id="${roomId} class=temperature-update">`),
+                `<input type="button" data-id="${roomId} value=Submit Changes" class="targetRoom">`);
 
 
+            $("#rooms-table").delegate(".targetRoom", "click", function (event) {
+                event.preventDefault();
+                let roomId = $(this).data("id");
+                let targetValue = $(this).siblings(".temperature-update").val();
+                if (targetValue == null || targetValue=== "") {
+                    console.log("Cannot update with null value");
+                    $(this).siblings(".temperature-update").val("");
+                }
+                else
+                    About.updateRoom( );
+
+
+            });
+        });
     },
-
 
 
     getHtmlForOneRoom: function (room) {
         return ` <tr>
             <td>${room.name}</td>
             <td>20</td>
-            <td>${room.targetValue}</td>
+            <td><input type="button" value="${room.targetValue}" data-id=${room.id}  class="target value"></td>
             <td><a href="#" data-id=${room.id} class="delete-room">Delete room</a></td>
-            <td>delete</td>
+            <td><a href="#" data-id=${room.name} class="edit-room">Edit</a></td>
         </tr>`
     },
 
-
-
-    getHtmlForOneTemperature: function (temperature) {
-        return ` <div class="temperatureControl">
-        <button>${temperature.targetValue}</button>
-    </div>`
-    }
 
 };
 About.getRooms();
