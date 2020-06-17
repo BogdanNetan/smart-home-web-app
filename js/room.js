6c6d44e96311d11c895e966db4f5077c80312742
BogdanNetan <netan.bogdan@gmail.com>
2020-06-17 17:40:05 +0300

    creating update method

--- js/room.js
+++ js/room.js
@@ -31,13 +31,15 @@ window.About = {
     },
 
 
-    updateRoom: function (id, name, targetValue) {
+    updateRoom: function (id, name) {
+        // let nameValue = $("#room-field").val();
+        // let targetValueValue = $("#target-field").val();
+
         let requestBody = {
-            name: name,
-            targetValue: targetValue
+            name: name
         };
         $.ajax({
-            url: About.API_URL + "/rooms" + "?id=" + id,
+            url: About.API_URL + "/rooms" + "/" + id,
             method: "PUT",
             contentType: "application/json",
             data: JSON.stringify(requestBody)
@@ -48,7 +50,7 @@ window.About = {
 
     deleteRoom: function (id) {
         $.ajax({
-            url: About.API_URL + "/rooms" + "/" + id,
+            url:About.API_URL + "/rooms/" + id,
             method: "DELETE",
             contentType: "application/json"
         }).done(function () {
@@ -71,41 +73,42 @@ window.About = {
             About.createRoom();
         });
 
-        $("#rooms-table ").delegate(".delete-room ", "click", function (event) {
+        $("#rooms-table").delegate(".delete-room ", "click", function (event) {
             event.preventDefault();
             let roomId = $(this).data("id");
             About.deleteRoom(roomId);
         });
-        // $("#rooms-table ").delegate(".edit-room", "click", function (event) {
-        //     event.preventDefault();
-        //     let roomId = $(this).data("id");
+        $("#rooms-table").delegate(".edit-room", "click", function (event) {
+            event.preventDefault();
+            let roomId = $(this).data("id");
+                About.updateRoom(roomId);
         //
         //
         //     $(this).replaceWith($(` <input type="text" data-id="${roomId}" placeholder="Enter new room name"/>`),
         //         `<input type="submit" value="Save" id=${roomId}  class="submitroomName-mark">`);
 
 
-        $("#rooms-table ").delegate(".Target value", "click", function (event) {
-            event.preventDefault();
-            let roomId = $(this).data("id");
+        // $("#rooms-table ").delegate(".Target value", "click", function (event) {
+        //     event.preventDefault();
+        //     let roomId = $(this).data("id");
 
-            $(this).replaceWith($(` <input type="text" placeholder="Target temperature" data-id="${roomId} class=temperature-update">`),
-                `<input type="button" data-id="${roomId} value=Submit Changes" class="targetRoom">`);
-
-
-            $("#rooms-table").delegate(".targetRoom", "click", function (event) {
-                event.preventDefault();
-                let roomId = $(this).data("id");
-                let targetValue = $(this).siblings(".temperature-update").val();
-                if (targetValue == null || targetValue=== "") {
-                    console.log("Cannot update with null value");
-                    $(this).siblings(".temperature-update").val("");
-                }
-                else
-                    About.updateRoom( );
+            // $(this).replaceWith($(` <input type="text" placeholder="Target temperature" data-id="${roomId} class=temperature-update">`),
+            //     `<input type="button" data-id="${roomId} value=Submit Changes" class="targetRoom">`);
 
 
-            });
+            // $("#rooms-table").delegate(".targetRoom", "click", function (event) {
+            //     event.preventDefault();
+            //     let roomId = $(this).data("id");
+            //     let targetValue = $(this).siblings(".temperature-update").val();
+            //     if (targetValue == null || targetValue=== "") {
+            //         console.log("Cannot update with null value");
+            //         $(this).siblings(".temperature-update").val("");
+            //     }
+            //     else
+            //         About.updateRoom( );
+            //
+            //
+            // });
         });
     },
 
@@ -116,7 +119,7 @@ window.About = {
             <td>20</td>
             <td><input type="button" value="${room.targetValue}" data-id=${room.id}  class="target value"></td>
             <td><a href="#" data-id=${room.id} class="delete-room">Delete room</a></td>
-            <td><a href="#" data-id=${room.name} class="edit-room">Edit</a></td>
+            <td><a href="#" data-id=${room.id} class="edit-room">Edit</a></td>
         </tr>`
     },
 
