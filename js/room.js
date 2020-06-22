0f258cb053ff73ffb586cd193a8c25776312b1d6
BogdanNetan <netan.bogdan@gmail.com>
2020-06-22 21:26:53 +0300

    creating update method

--- js/room.js
+++ js/room.js
@@ -32,21 +32,21 @@ window.About = {
 
 
     updateRoom: function (id, name) {
-        // let nameValue = $("#room-field").val();
-        // let targetValueValue = $("#target-field").val();
+        let nameValue = $("#room-field").val();
+        let targetValueValue = $("#target-field").val();
 
         let requestBody = {
-            name: name
+            name: nameValue,
+            targetValue:targetValueValue
         };
         $.ajax({
-            url: About.API_URL + "/rooms" + "/" + id,
-            method: "PUT",
-            contentType: "application/json",
-            data: JSON.stringify(requestBody)
-        }).done(function () {
-            About.getRooms();
-        })
-    },
+            url: About.API_URL + '/rooms/' + id,
+            method: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(requestBody),
+            dataType: 'json'
+        }).done(function(data) {console.log(data)}).fail(function(xhr, status, error) {console.log(status); console.log(error)})
+},
 
     deleteRoom: function (id) {
         $.ajax({
@@ -81,34 +81,24 @@ window.About = {
         $("#rooms-table").delegate(".edit-room", "click", function (event) {
             event.preventDefault();
             let roomId = $(this).data("id");
+
+            $(this).replaceWith($(` <input type="text" data-id="${roomId}" placeholder="Enter new room name"/>`),
+                        `<input type="submit" value="Save" id=${roomId}  class="submitroomName-mark">`);
+
+
+                    // $("#rooms-table").delegate(".targetRoom", "click", function (event) {
+                    //     event.preventDefault();
+                    //     let roomId = $(this).data("id");
+                    //     let targetValue = $(this).siblings(".temperature-update").val();
+                    //     if (targetValue == null || targetValue=== "") {
+                    //         console.log("Cannot update with null value");
+                    //         $(this).siblings(".temperature-update").val("");
+                    //     }
+                    //     else
+
+
+
                 About.updateRoom(roomId);
-        //
-        //
-        //     $(this).replaceWith($(` <input type="text" data-id="${roomId}" placeholder="Enter new room name"/>`),
-        //         `<input type="submit" value="Save" id=${roomId}  class="submitroomName-mark">`);
-
-
-        // $("#rooms-table ").delegate(".Target value", "click", function (event) {
-        //     event.preventDefault();
-        //     let roomId = $(this).data("id");
-
-            // $(this).replaceWith($(` <input type="text" placeholder="Target temperature" data-id="${roomId} class=temperature-update">`),
-            //     `<input type="button" data-id="${roomId} value=Submit Changes" class="targetRoom">`);
-
-
-            // $("#rooms-table").delegate(".targetRoom", "click", function (event) {
-            //     event.preventDefault();
-            //     let roomId = $(this).data("id");
-            //     let targetValue = $(this).siblings(".temperature-update").val();
-            //     if (targetValue == null || targetValue=== "") {
-            //         console.log("Cannot update with null value");
-            //         $(this).siblings(".temperature-update").val("");
-            //     }
-            //     else
-            //         About.updateRoom( );
-            //
-            //
-            // });
         });
     },
 
